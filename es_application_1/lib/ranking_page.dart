23ac135fa3635cb3df1c8db186db9944ca352fd8
GonçaloMatias <up202108703@up.pt>
2024-04-20 22:22:58 +0100

    ranking page created

---
+++ es_application_1/lib/ranking_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RankingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Rankings'),
+      ),
+      body: Center(
+        child: Text('Ranking will be displayed here.'),
+      ),
+    );
+  }
+}
