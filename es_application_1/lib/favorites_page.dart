f1a2b0644bb3b6c17b6552ee172544b71ab255d9
GonçaloMatias <up202108703@up.pt>
2024-05-06 22:16:09 +0100

    favs to fav page working

--- es_application_1/lib/favorites_page.dart
+++ es_application_1/lib/favorites_page.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'profile_page.dart';
 import 'create_post/create_post.dart';
 import 'main_page.dart';
 
-class FavoritesPage extends StatelessWidget {
+class FavoritesPage extends StatefulWidget {
+  @override
+  _FavoritesPageState createState() => _FavoritesPageState();
+}
+
+class _FavoritesPageState extends State<FavoritesPage> {
+  List<DocumentSnapshot> favoritePosts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchFavoritePosts();
+  }
+
+  fetchFavoritePosts() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final userData = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+      List<dynamic> favoriteIds = userData.data()?['favorites'] as List<dynamic>? ?? [];
+      List<Future<DocumentSnapshot>> postFutures = [];
+
+      for (String id in favoriteIds) {
+        postFutures.add(FirebaseFirestore.instance.collection('posts').doc(id).get());
+      }
+
+      favoritePosts = await Future.wait(postFutures);
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,8 +42,17 @@ class FavoritesPage extends StatelessWidget {
         title: const Text('Favorites'),
         backgroundColor: Colors.green,
       ),
-      body: const Center(
-        child: Text('Still empty'),
+      body: favoritePosts.isEmpty
+          ? const Center(child: Text('No favorites added yet.'))
+          : ListView.builder(
+        itemCount: favoritePosts.length,
+        itemBuilder: (context, index) {
+          Map<String, dynamic> postData = favoritePosts[index].data() as Map<String, dynamic>;
+          return ListTile(
+            title: Text(postData['activityName'] ?? 'No title available'),
+            subtitle: Text(postData['description'] ?? 'No description available'),
+          );
+        },
       ),
       bottomNavigationBar: buildBottomNavigationBar(context),
     );
