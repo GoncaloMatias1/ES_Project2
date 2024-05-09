9bf5317de8953a8d5a3c6b7d96d810af74526646
GonçaloMatias <up202108703@up.pt>
2024-05-09 22:47:48 +0100

    Update ranking_page.dart

--- es_application_1/lib/ranking_page.dart
+++ es_application_1/lib/ranking_page.dart
@@ -1,7 +1,8 @@
-import 'package:es_application_1/profile_page.dart';
 import 'package:flutter/material.dart';
+import 'profile_page.dart';
 import 'create_post/create_post.dart';
 import 'main_page.dart';
+import 'favorites_page.dart';
 
 class RankingPage extends StatelessWidget {
   const RankingPage({super.key});
@@ -11,24 +12,38 @@ class RankingPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Rankings'),
+        backgroundColor: Colors.green,
       ),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (BuildContext context, int index) {
-          final ranking = 'Rank ${index + 1}';
-          final score = (index + 1) * 100;
-
-          return ListTile(
-            leading: CircleAvatar(
-              child: Text((index + 1).toString()),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Your current ranking is: #XX',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.green),
             ),
-            title: Text(ranking),
-            subtitle: Text('Score: $score'),
-            trailing: const Icon(Icons.arrow_forward),
-            onTap: () {
-            },
-          );
-        },
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (BuildContext context, int index) {
+                final ranking = 'Rank ${index + 1}';
+                final score = (index + 1) * 100;
+
+                return ListTile(
+                  leading: CircleAvatar(
+                    child: Text((index + 1).toString()),
+                  ),
+                  title: Text(ranking),
+                  subtitle: Text('Score: $score'),
+                  trailing: const Icon(Icons.arrow_forward),
+                  onTap: () {
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.green,
@@ -52,6 +67,29 @@ class RankingPage extends StatelessWidget {
                   ),
                   child: const Icon(
                     Icons.home,
+                    color: Colors.green,
+                    size: 30.0,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => FavoritesPage()),
+                  );
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                  child: const Icon(
+                    Icons.favorite,
                     color: Colors.green,
                     size: 30.0,
                   ),
@@ -104,30 +142,6 @@ class RankingPage extends StatelessWidget {
                 ),
               ),
             ),
-            if (ModalRoute.of(context)!.settings.name != '/ranking')
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: InkWell(
-                  onTap: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => const RankingPage()),
-                    );
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.all(8.0),
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
-                    ),
-                    child: const Icon(
-                      Icons.leaderboard,
-                      color: Colors.green,
-                      size: 30.0,
-                    ),
-                  ),
-                ),
-              ),
           ],
         ),
       ),
